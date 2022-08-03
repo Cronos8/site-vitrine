fc267a9493cf75faacf308df4ccdfff24239553c
Cronos8 <alexng.tp@gmail.com>
2022-08-02 20:56:50 -0400

    add tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <button class="px-6 py-2 text-sm text-green-100 bg-indigo-600 rounded">Next</button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -31,12 +32,12 @@
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
